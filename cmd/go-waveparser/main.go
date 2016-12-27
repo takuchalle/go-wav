32901d845d571a19f9b02dba3fce1685eef77112
Takuya OHASHI <tky.ohashi@gmail.com>
2016-12-27 14:23:38 +0900

    use arg for input wave file

--- cmd/go-waveparser/main.go
+++ cmd/go-waveparser/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"github.com/takuyaohashi/go-waveparser"
 )
 
+func usage() {
+	fmt.Printf("Usage: go-waveparser [wave file name]\n");
+	os.Exit(1)
+}
+
 func main() {
-	fmt.Printf("Hello, World\n");
+	if(len(os.Args) != 2) { usage() }
+	
+	f, err := os.Open(os.Args[1]) // For read access.
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	parser := waveparser.New(f)
+	parser.Parse()
+	header := parser.GetHeader()
+	fmt.Printf("chunk_id = %d\n", header.GetChunkId());
 }
