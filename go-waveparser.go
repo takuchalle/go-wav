525de5400146239e299f1beeba9c6ddc93daab70
Takuya OHASHI <tky.ohashi@gmail.com>
2016-12-28 14:09:38 +0900

    read from io.Reader

--- go-waveparser.go
+++ go-waveparser.go
@@ -2,6 +2,8 @@ package waveparser
 
 import (
 	"io"
+	"log"
+	"fmt"
 )
 
 type WaveHeader struct {
@@ -22,15 +24,22 @@ type WaveHeader struct {
 
 type WaveParser struct {
 	header WaveHeader
+	reader io.Reader
 }
 
 func New(r io.Reader) *WaveParser {
-	header := &WaveParser{}
-	return header
+	parser := &WaveParser{}
+	parser.reader = r
+	return parser
 }
 
 func (parser *WaveParser) Parse() {
-	
+	buffer := make([]byte , 100)
+	_, err := io.ReadAtLeast(parser.reader, buffer, 100)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%v\n",buffer)
 }
 
 func (parser *WaveParser) GetHeader() *WaveHeader {
@@ -40,3 +49,8 @@ func (parser *WaveParser) GetHeader() *WaveHeader {
 func (header *WaveHeader) GetChunkId() int {
 	return header.chunk_id
 }
+
+func (header *WaveHeader) GetChunkSize() int {
+	return header.chunk_size
+}
+
