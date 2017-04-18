ad9693a0f383b46924d48436361b8a9be05fefed
Takuya OHASHI <tky.ohashi@gmail.com>
2017-04-18 20:34:38 +0900

    :tada: Release Command

--- cmd/go-waveparser/main.go
+++ cmd/go-waveparser/main.go
@@ -25,5 +25,14 @@ func main() {
 	parser := waveparser.New(f)
 	parser.Parse()
 	header := parser.GetHeader()
-	fmt.Printf("chunk_id = %d\n", header.GetChunkId())
+	fmt.Printf("Format \t\t= %s\n", header.GetFormat())
+	fmt.Printf("Sub Chunk Size \t= %d\n", header.GetSubChunkSize())
+	fmt.Printf("Audio Format \t= %d\n", header.GetAudioFormat())
+	fmt.Printf("Num Of Channels = %d\n", header.GetNumChannels())
+	fmt.Printf("Sample Rate \t= %d\n", header.GetSampleRate())
+	fmt.Printf("Byte Rate \t= %d\n", header.GetByteRate())
+	fmt.Printf("Block Align \t= %d\n", header.GetBlockAlign())
+	fmt.Printf("Bit Per Sample \t= %d\n", header.GetBitPerSample())
+	fmt.Printf("Data Size \t= %d\n", header.GetSubChunk2Size())
 }
+
