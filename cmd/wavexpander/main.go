695e69808ae1618a001f800d3cc49dab2a481f85
Takuya OHASHI <tky.ohashi@gmail.com>
2017-05-25 11:08:20 +0900

    expand 24bit -> 32bit
     - 0 padding for first byte

--- cmd/wavexpander/main.go
+++ cmd/wavexpander/main.go
@@ -13,6 +13,23 @@ func usage() {
 	fmt.Println("Usage: go-wavexpand [wav file]")
 }
 
+func expand(in <-chan []byte) <-chan []byte{
+	out := make(chan []byte)
+
+	go func() {
+		defer close(out)
+		for data := range in {
+			buf := make([]byte, 4)
+			for i := 0; i < len(data); i++ {
+				buf[i+1] = data[i]
+			}
+			out <- buf
+		}
+	}()
+	
+	return out
+}
+
 func read(reader *bufio.Reader) <-chan []byte {
 	out := make(chan []byte)
 
@@ -62,8 +79,11 @@ func main() {
 
 	reader := bufio.NewReaderSize(f, int(header.SubChunk2Size))
 	out := read(reader)
+	expand := expand(out)
 
-	for i := range out {
+	for i := range expand {
 		writer.Write(i)
 	}
+
+	writer.Flush()
 }
