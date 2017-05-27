c6a7911319bb028cf92356b1b5b1975f87800afb
Takuya OHASHI <tky.ohashi@gmail.com>
2017-05-27 22:31:11 +0900

    create run function

--- cmd/wavexpander/main.go
+++ cmd/wavexpander/main.go
@@ -14,7 +14,7 @@ func usage() {
 	os.Exit(1)
 }
 
-func expand(in <-chan []byte) <-chan []byte{
+func expand(in <-chan []byte) <-chan []byte {
 	out := make(chan []byte)
 
 	go func() {
@@ -27,7 +27,7 @@ func expand(in <-chan []byte) <-chan []byte{
 			out <- buf
 		}
 	}()
-	
+
 	return out
 }
 
@@ -48,7 +48,7 @@ func read(reader *bufio.Reader) <-chan []byte {
 	return out
 }
 
-func main() {
+func run() int {
 	if len(os.Args) != 2 {
 		usage()
 	}
@@ -56,7 +56,7 @@ func main() {
 	f, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return 1
 	}
 	defer f.Close()
 
@@ -64,19 +64,17 @@ func main() {
 	err = parser.Parse()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return 1
 	}
 	header := parser.GetHeader()
 
 	wf, err2 := os.Create("hoge.wav")
 	if err2 != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return 1
 	}
 	defer wf.Close()
 	writer := bufio.NewWriter(wf)
-
-	fmt.Printf("size = %d\n", header.SubChunk2Size)
 
 	reader := bufio.NewReaderSize(f, int(header.SubChunk2Size))
 	out := read(reader)
@@ -87,4 +85,12 @@ func main() {
 	}
 
 	writer.Flush()
+
+	return 0
+}
+
+func main() {
+	if run() != 0 {
+		os.Exit(1)
+	}
 }
