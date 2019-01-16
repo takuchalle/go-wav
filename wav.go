4021433230fc77699134d5f7ea840094d8eb250f
Takuya OHASHI <tky.ohashi@gmail.com>
2019-01-16 18:24:35 +0900

    Add Open function instead of NewWav

--- wav.go
+++ wav.go
@@ -2,17 +2,30 @@ package wav
 
 import (
 	"io"
+	"os"
 )
 
-// Wav is
+// Wav is wav file struct.
 type Wav struct {
 	header WaveHeader
 	reader io.ReadSeeker
 }
 
-// NewWav creats Wave Parser
-func NewWav(r io.ReadSeeker) *Wav {
-	parser := &Wav{}
-	parser.reader = r
-	return parser
+// Open opens Wav file
+func Open(name string) (*Wav, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	w := &Wav{}
+	w.reader = f
+
+	err = w.Parse()
+	if err != nil {
+		return nil, err
+	}
+
+	return w, err
 }
