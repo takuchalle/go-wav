d1023e10b6c6ab10a012ed9357da82a28ea6094b
Takuya OHASHI <tky.ohashi@gmail.com>
2019-01-17 15:32:42 +0900

    Display channel num

--- cmd/wavparser/main.go
+++ cmd/wavparser/main.go
@@ -17,17 +17,16 @@ func main() {
 		usage()
 	}
 
-	w, err := wav.Open(os.Args[1])
+	f, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	header := w.GetHeader()
-	fmt.Printf("Sub Chunk Size \t= %d\n", header.SubChunkSize)
-	fmt.Printf("Audio Format \t= %d\n", header.AudioFormat)
-	fmt.Printf("Num Of Channels = %d\n", header.NumChannels)
-	fmt.Printf("Sample Rate \t= %d\n", header.SampleRate)
-	fmt.Printf("Byte Rate \t= %d\n", header.ByteRate)
-	fmt.Printf("Block Align \t= %d\n", header.BlockAlign)
-	fmt.Printf("Bit Per Sample \t= %d\n", header.BitsPerSample)
-	fmt.Printf("Data Size \t= %d\n", header.SubChunk2Size)
+	defer f.Close()
+
+	w, err := wav.NewReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Num of channles is %v\n", w.GetNumChannels())
 }
