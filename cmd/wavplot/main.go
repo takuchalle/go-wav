f6054ec069dabde5b3b2a10f2fb883b51c88fab3
Takuya OHASHI <tky.ohashi@gmail.com>
2019-01-29 19:27:58 +0900

    [WIP] add wavplot

---
+++ cmd/wavplot/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/takuyaohashi/go-wav"
+	"github.com/takuyaohashi/go-wav/wavplotter"
+)
+
+func usage() {
+	log.Fatal("Usage: wavplot [wave file name]")
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		usage()
+	}
+
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	
+	w, err := wav.NewReader(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	plotter := wavplotter.NewPlotter(w)
+	plotter.Plot()
+}
