433d94c47796dde6e9ea257e789e060812da4146
Takuya OHASHI <tky.ohashi@gmail.com>
2019-02-06 17:07:22 +0900

    write Wav Header

--- errors.go
+++ errors.go
@@ -9,4 +9,5 @@ var (
 	ErrNotWavFile = errors.New("Not a Wav file")
 	ErrNoFmt      = errors.New("No fmt word")
 	ErrInvalidFmt = errors.New("Audio Format Error")
+	ErrFailedWrite = errors.New("Failet to write")
 )
--- writer.go
+++ writer.go
@@ -15,27 +15,126 @@ type WriterParam struct {
 }
 
 type Writer struct {
-	r io.Writer
+	r io.WriteSeeker
 
 	p WriterParam
 }
 
-func NewWriter(r io.Writer, p WriterParam) (w *Writer, err error) {
+func NewWriter(r io.WriteSeeker, p WriterParam) (w *Writer, err error) {
+	if p.SampleRate == 0 || p.BitsPerSample == 0 || p.NumChannels == 0 {
+		return nil, ErrInvalidFmt
+	}
+
 	w = &Writer{r: r, p: p}
+	w.writeHeader()
 	return w, nil
+}
+
+func (w *Writer) writeRiffChunk(buf *bytes.Buffer) error {
+	header := fmt.Sprintf("RIFF")
+	err := binary.Write(buf, binary.LittleEndian, []byte(header))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// Chunk Size
+	err = binary.Write(buf, binary.LittleEndian, float32(0))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	header = fmt.Sprintf("WAVE")
+	err = binary.Write(buf, binary.LittleEndian, []byte(header))
+	if err != nil {
+		return ErrFailedWrite
+	}
+	return nil
+}
+
+func (w *Writer) writeFmtSubChunk(buf *bytes.Buffer) error {
+
+	header := fmt.Sprintf("fmt ")
+	err := binary.Write(buf, binary.LittleEndian, []byte(header))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// SubChunk1Size
+	err = binary.Write(buf, binary.LittleEndian, int32(00000))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// AudioFormat
+	err = binary.Write(buf, binary.LittleEndian, int16(1))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// Num Channels
+	err = binary.Write(buf, binary.LittleEndian, int16(w.p.NumChannels))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// Sample Rate
+	err = binary.Write(buf, binary.LittleEndian, int32(w.p.SampleRate))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// Byte Rate
+	byteRate := w.p.SampleRate * uint32(w.p.NumChannels) * uint32(w.p.BitsPerSample) / 8
+	err = binary.Write(buf, binary.LittleEndian, int32(byteRate))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// Block Align
+	blockAlign := uint32(w.p.NumChannels) * uint32(w.p.BitsPerSample) / 8
+	err = binary.Write(buf, binary.LittleEndian, int16(blockAlign))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// Bits Per Sample
+	err = binary.Write(buf, binary.LittleEndian, int16(w.p.BitsPerSample))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// data
+	header = fmt.Sprintf("data")
+	err = binary.Write(buf, binary.LittleEndian, []byte(header))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	// SubChunk2Size
+	err = binary.Write(buf, binary.LittleEndian, int32(4))
+	if err != nil {
+		return ErrFailedWrite
+	}
+
+	return nil
 }
 
 func (w *Writer) writeHeader() {
 	buf := new(bytes.Buffer)
-	header := fmt.Sprintf("RIFFWAVEfmt")
-	err := binary.Write(buf, binary.LittleEndian, []byte(header))
+
+	err := w.writeRiffChunk(buf)
 	if err != nil {
 		fmt.Println("binary.Write failed:", err)
 	}
-	fmt.Printf("%x\n", buf.Bytes())
+
+	err = w.writeFmtSubChunk(buf)
+	if err != nil {
+		fmt.Println("binary.Write failed:", err)
+	}
+
 	w.r.Write(buf.Bytes())
 }
 
-func (w *Writer) Write() {
-	w.writeHeader()
+func (w *Writer) WriteSamples() {
+
 }
