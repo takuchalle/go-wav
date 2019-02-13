3d6c7c9313ea96fc709b49db049a6ae960297c24
Takuya OHASHI <tky.ohashi@gmail.com>
2019-02-13 17:01:41 +0900

    :lipstick: gofmt

--- errors.go
+++ errors.go
@@ -5,9 +5,9 @@ import (
 )
 
 var (
-	ErrNoRIFF     = errors.New("No RIFF word")
-	ErrNotWavFile = errors.New("Not a Wav file")
-	ErrNoFmt      = errors.New("No fmt word")
-	ErrInvalidFmt = errors.New("Audio Format Error")
+	ErrNoRIFF      = errors.New("No RIFF word")
+	ErrNotWavFile  = errors.New("Not a Wav file")
+	ErrNoFmt       = errors.New("No fmt word")
+	ErrInvalidFmt  = errors.New("Audio Format Error")
 	ErrFailedWrite = errors.New("Failet to write")
 )
