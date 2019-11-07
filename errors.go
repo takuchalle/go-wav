1655385446ecdb510551bd9e582a16fc9ffbc71f
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-07 16:15:32 +0900

    Fix warning

--- errors.go
+++ errors.go
@@ -5,9 +5,18 @@ import (
 )
 
 var (
-	ErrNoRIFF      = errors.New("No RIFF word")
-	ErrNotWavFile  = errors.New("Not a Wav file")
-	ErrNoFmt       = errors.New("No fmt word")
-	ErrInvalidFmt  = errors.New("Audio Format Error")
+	// ErrNoRIFF is the error about missing RIFF word.
+	ErrNoRIFF = errors.New("No RIFF word")
+
+	// ErrNotWavFile is the error that input file is not wav file.
+	ErrNotWavFile = errors.New("Not a Wav file")
+
+	// ErrNoFmt is the error about missing WAV word.
+	ErrNoFmt = errors.New("No fmt word")
+
+	// ErrInvalidFmt is the error about fomat error.
+	ErrInvalidFmt = errors.New("Audio Format Error")
+
+	// ErrFailedWrite is the error about failing to write.
 	ErrFailedWrite = errors.New("Failet to write")
 )
