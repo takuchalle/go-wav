7f10cb409832523fa48ac18f120426af41c1f65c
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-08 15:45:56 +0900

    Update generate sin wave command

--- cmd/singen/main.go
+++ cmd/singen/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"os"
 
@@ -8,14 +9,41 @@ import (
 	"github.com/urfave/cli"
 )
 
-func generate_sin(c *cli.Context, w *wav.Writer) error {
+func generateAction(c *cli.Context) error {
+	outputFile := c.String("output")
+	rate := c.Int("rate")
+	amp := c.Float64("amp")
+	freq := c.Int("f")
+	sec := c.Float64("sec")
 
-	length := c.Float64("sec") * c.Float64("rate")
+	fmt.Printf("Output file: %s\n", outputFile)
+	fmt.Printf("sampling rate: %d\n", rate)
+	fmt.Printf("amplitude: %f\n", amp)
+	fmt.Printf("freq: %d Hz\n", freq)
+	fmt.Printf("time: %f sec\n", sec)
+
+	f, err := os.Create(outputFile)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+	defer f.Close()
+
+	var param = wav.WriterParam{SampleRate: uint32(rate), BitsPerSample: 16, NumChannels: 1, AudioFormat: wav.AudioFormatPCM}
+
+	w, err := wav.NewWriter(f, param)
+	if err != nil {
+		return cli.NewExitError(err, 1)
+	}
+	defer w.Close()
+
+	length := sec * float64(rate)
+	delta := float64(freq) / float64(rate)
+	pi2 := math.Pi * 2.0
 
 	for i := 0; i < int(length); i += 256 {
 		samples := make([]int16, 256)
 		for j := 0; j < 256; j++ {
-			data := c.Float64("amp") * math.Sin(math.Pi*2.0*float64(i+j)*(c.Float64("f"))/c.Float64("rate"))
+			data := amp * math.Sin(pi2*float64(i+j)*delta)
 			if data > 1.0 {
 				data = 1.0
 			}
@@ -26,26 +54,6 @@ func generate_sin(c *cli.Context, w *wav.Writer) error {
 		}
 		w.WriteSamples(samples)
 	}
-
-	return nil
-}
-
-func generate(c *cli.Context) error {
-	f, err := os.Create(c.String("output"))
-	if err != nil {
-		return cli.NewExitError(err, 1)
-	}
-	defer f.Close()
-
-	samplerate := uint32(c.Int("rate"))
-	w, err := wav.NewWriter(f, wav.WriterParam{SampleRate: samplerate, BitsPerSample: 16, NumChannels: 1, AudioFormat: wav.AudioFormatPCM})
-	if err != nil {
-		return cli.NewExitError(err, 1)
-	}
-
-	generate_sin(c, w)
-
-	w.Close()
 
 	return nil
 }
@@ -77,7 +85,7 @@ func main() {
 
 		cli.IntFlag{
 			Name:  "freq, f",
-			Value: 100,
+			Value: 1000,
 			Usage: "sin frequency",
 		},
 
@@ -87,6 +95,6 @@ func main() {
 			Usage: "sampling rate",
 		},
 	}
-	app.Action = generate
+	app.Action = generateAction
 	app.Run(os.Args)
 }
